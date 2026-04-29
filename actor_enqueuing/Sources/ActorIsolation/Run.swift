05215f4e20939969d32820ce159b27e9a556dc3b
Denny Mathew <devdenmathew@gmail.com>
2026-04-29 10:12:37 +0200

    actor_enqueuing

--- actor_enqueuing/Sources/ActorIsolation/Run.swift
+++ actor_enqueuing/Sources/ActorIsolation/Run.swift
@@ -1,5 +1,5 @@
 extension Actor {
-  public func run<R, Failure: Error>(_ body: @Sendable (isolated Self) throws(Failure) -> R) throws(Failure) -> R {
-    try body(self)
-  }
+    public func run<R, Failure: Error>(_ body: @Sendable (isolated Self) throws(Failure) -> R) throws(Failure) -> R {
+        try body(self)
+    }
 }
